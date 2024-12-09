a831310cf9bd522adf7696e4a14d150c881513be
AriB2003 <ari.bobesh@gmail.com>
2024-12-08 23:48:30 -0500

    Got image fetching, resizing, and rotating

--- design_templates.py
+++ design_templates.py
@@ -17,3 +17,90 @@ m2 = np.array(
         [0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0],
     ]
 )
+
+urls = [
+    {
+        "url": "https://d.newsweek.com/en/full/2463914/haliey-welch.jpg?w=1200&f=daddd6871e66c2facf472a455c0a94d6",
+        "method": "default",
+        "parameters": (1.3, 5),
+        "quantity": 1,
+    },
+    {
+        "url": "https://www.rollingstone.com/wp-content/uploads/2024/12/hailey-welch-hawk-memecoin.jpg?w=1581&h=1054&crop=1",
+        "method": "default",
+        "parameters": (1.3, 5),
+        "quantity": 1,
+    },
+    {
+        "url": "https://content.api.news/v3/images/bin/7a30da19649e6f81aac060bdaa9f59c3",
+        "method": "alternative",
+        "parameters": (1.05, 3),
+        "quantity": 1,
+    },
+    {
+        "url": "https://www.pajiba.com/assets_c/2024/12/hawktuahscam-thumb-700xauto-267027.png",
+        "method": "default",
+        "parameters": (1.3, 5),
+        "quantity": 1,
+    },
+    {
+        "url": "https://i.ytimg.com/vi/JKGT6T4R3Pg/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBJZxg3SVov1N9K4Dm0H2AHpCehHw",
+        "method": "default",
+        "parameters": (1.3, 5),
+        "quantity": 1,
+    },
+    {
+        "url": "https://townsquare.media/site/204/files/2024/07/attachment-HawkTuah.jpg",
+        "method": "alternative",
+        "parameters": (1.1, 3),
+        "quantity": 2,
+    },
+    {
+        "url": "https://preview.redd.it/whats-the-likelihood-of-the-hawk-tuah-girl-being-on-next-v0-hsm9dfnudved1.jpeg?auto=webp&s=256567b55894a7288be5b7a596e12253a1208c4a",
+        "method": "default",
+        "parameters": (1.3, 5),
+        "quantity": 1,
+    },
+    {
+        "url": "https://www.tampabay.com/resizer/v2/haliey-welch-also-known-as-hawk-tuah-girl-GUE57I7KGTDRYBUIWOX4ZAJXGU.jpg?auth=1445b42ab0feca28b8dacb51e24a18ebd481b4c3032f0f9496a591859c95edbd&height=506&width=900&smart=true",
+        "method": "default",
+        "parameters": (1.3, 5),
+        "quantity": 1,
+    },
+    {
+        "url": "https://www.hollywoodreporter.com/wp-content/uploads/2024/07/0U9A2440.jpg?w=1296",
+        "method": "alternative",
+        "parameters": (1.05, 3),
+        "quantity": 1,
+    },
+    {
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHhQWN-6jlDMgtCC2BxowaI7U1q15XDxjskg&s",
+        "method": "default",
+        "parameters": (1.3, 5),
+        "quantity": 1,
+    },
+    {
+        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW9f0DEiT5nNAuD6Il3_s4YYZREFmQ_9b1tQ&s",
+        "method": "alternative",
+        "parameters": (1.05, 3),
+        "quantity": 1,
+    },
+    {
+        "url": "https://thespun.com/.image/t_share/MjA4MTA1MDE4MTAyOTE2NzUy/screenshot-2024-07-25-at-33956pm.png",
+        "method": "alternative",
+        "parameters": (1.05, 3),
+        "quantity": 1,
+    },
+    {
+        "url": "https://people.com/thmb/FiEWf4Vz9DA-R8V18nXtCzeDexo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(776x234:778x236)/Haliey-Welch-Hawk-Tuah-girl-091624-tout-ffd7f3e928254867988657a6b18ab642.jpg",
+        "method": "alternative",
+        "parameters": (1.02, 3),
+        "quantity": 1,
+    },
+    {
+        "url": "https://thespun.com/.image/t_share/MjEwNDUzNTQ4OTAxNjA3NDAx/screenshot-2024-11-03-at-85430pm.png",
+        "method": "default",
+        "parameters": (1.3, 5),
+        "quantity": 1,
+    },
+]
--- spot_it_generator.py
+++ spot_it_generator.py
@@ -1,9 +1,150 @@
 import numpy as np
-from design_templates import m2
+import requests
+import cv2 as cv
+import os
+import math
+import random
+from design_templates import m2, urls
+
+
+def template_symbols(design):
+    return np.sum(design, axis=0)
 
 
 def symbols_per_card(design):
-    return np.max(np.sum(design, axis=0))
+    return np.max(template_symbols(design))
 
 
-print(symbols_per_card(m2))
+def total_symbols(design):
+    tp = template_symbols(design)
+    unique = symbols_per_card(design) * len(tp) - np.sum(tp)
+    matching = np.size(design, 1)
+    return unique + matching
+
+
+def area_sort(face):
+    return face[2] * face[3]
+
+
+def fetch_images(urls):
+    face_cascade_default = cv.CascadeClassifier(
+        cv.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+    # face_cascade_alternative1 = cv.CascadeClassifier(
+    #     cv.data.haarcascades + "haarcascade_frontalface_alt.xml"
+    # )
+    face_cascade_alternative = cv.CascadeClassifier(
+        cv.data.haarcascades + "haarcascade_frontalface_alt2.xml"
+    )
+    face_cascade_profile = cv.CascadeClassifier(
+        cv.data.haarcascades + "haarcascade_profileface.xml"
+    )
+    counter = 0
+    for i, url in enumerate(urls):
+        img_data = requests.get(url["url"]).content
+        print(f"Got: {url["url"]}")
+        with open("temp.png", "wb") as f:
+            f.write(img_data)
+        img = cv.imread("temp.png")
+        os.remove("temp.png")
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        faces = []
+        if url["method"] == "default":
+            faces = face_cascade_default.detectMultiScale(gray, 1.3, 5)
+        elif url["method"] == "alternative":
+            faces = face_cascade_alternative.detectMultiScale(
+                gray, url["parameters"][0], url["parameters"][1]
+            )
+        elif url["method"] == "profile":
+            faces = face_cascade_profile.detectMultiScale(
+                gray, url["parameters"][0], url["parameters"][1]
+            )
+        if len(faces) == 0:
+            print("No faces found!")
+            # exit(0)
+        else:
+            print(f"{len(faces)} faces found!")
+        faces = faces.tolist()
+        faces.sort(key=area_sort, reverse=True)
+        for j, (x, y, w, h) in enumerate(faces[: url["quantity"]]):
+            h = w
+            offset = min(100, x, y, img.shape[1] - x - w, img.shape[0] - y - h)
+            print(f"Calculated Offset: {offset}")
+            roi_color = img[y - offset : y + h + offset, x - offset : x + w + offset]
+            mask = np.zeros(roi_color.shape, dtype=np.uint8)
+            mask2 = 255 * np.ones(roi_color.shape, dtype=np.uint8)
+            # Define the center and radius of the circle
+            center = (roi_color.shape[1] // 2, roi_color.shape[0] // 2)
+            radius = min(roi_color.shape[1] // 2, roi_color.shape[0] // 2)
+            # Draw a white circle on the mask
+            cv.circle(mask, center, radius, 255, -1)
+            cv.circle(mask2, center, radius, 0, -1)
+            # Apply the mask to the image
+            masked_image = cv.bitwise_and(roi_color, mask)
+            masked_image = cv.bitwise_or(masked_image, mask2)
+            counter += 1
+            save_path = os.path.join(".", "source_images", f"talktuah{counter}.png")
+            cv.imwrite(save_path, masked_image)
+            print(f"Saved Image: {save_path}")
+    return True
+
+
+def resize_images(image_directory, scale):
+    min_size = math.inf
+    for path in image_directory:
+        img = cv.imread(os.path.join(".", "source_images", path))
+        min_size = min(img.shape[0], min_size)
+    if scale:
+        min_size *= 2
+    for path in image_directory:
+        fp = os.path.join(".", "source_images", path)
+        img = cv.imread(fp)
+        cv.imwrite(fp, cv.resize(img, (min_size, min_size)))
+        print(f"Resized {path} to {(min_size, min_size)}")
+
+
+def rotate_image(image, angle):
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rot_mat = cv.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv.INTER_LINEAR)
+    mask = 255 * np.ones(result.shape, dtype=np.uint8)
+    center = (result.shape[1] // 2, result.shape[0] // 2)
+    radius = min(result.shape[1] // 2, result.shape[0] // 2)
+    cv.circle(mask, center, radius, 0, -1)
+    result = cv.bitwise_or(result, mask)
+    return result
+
+
+def generate_valid_location(origin, locations, distance):
+    
+
+
+def create_canvas(image_directory, num_symbols):
+    shape = cv.imread(os.path.join(".", "source_images", image_directory[0]))
+    locations = [(0,0)]
+    generate_valid_location(locations[0],locations, shape[0])
+    canvas = np.zeros([3 * shape[0], 3 * shape[1], 3], dtype=np.uint8)
+    center = (canvas.shape[1] // 2, canvas.shape[0] // 2)
+    radius = min(canvas.shape[1] // 2, canvas.shape[0] // 2)
+    cv.circle(canvas, center, radius, 255, -1)
+    return canvas, locations
+
+
+scale = False
+# Uncomment to fetch images
+# scale = fetch_images(urls)
+
+total = total_symbols(m2)
+print(f"Required Symbols: {total}")
+
+image_directory = os.listdir(os.path.join(".", "source_images"))
+print(f"Found Images:\n{"\n".join(image_directory)}")
+
+if len(image_directory) < total:
+    print("Not enough images to generate")
+    exit(0)
+
+resize_images(image_directory, scale)
+
+canvas, locations = create_canvas(image_directory, symbols_per_card(m2))
+cv.imwrite("hi.png", canvas)
